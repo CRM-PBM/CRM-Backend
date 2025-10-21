8afbe238f5457e854a5287732fb015dcfc8f3d8c
MAsfaDevano72 <masfadevano72@gmail.com>
2025-10-21 16:52:43 +0700

    Filter Produk sesuai umkm_id + buat kode produk

--- src/models/Produk.js
+++ src/models/Produk.js
@@ -1,15 +1,17 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
-const JenisProduk = require('./JenisProduk')
+const JenisProduk = require("./JenisProduk");
+const Umkm = require("./Umkm");
 
 const Produk = sequelize.define("Produk", {
   produk_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  nama_produk: { type: DataTypes.STRING(100), allowNull: false },
+  nama_produk: { type: DataTypes.STRING(50), allowNull: false },
   kode_produk: { type: DataTypes.STRING(20), unique: true, allowNull: true },
-  jenis_produk_id: { type: DataTypes.INTEGER, allowNull: true },
+  jenis_produk_id: { type: DataTypes.INTEGER, allowNull: false },
   harga: { type: DataTypes.DECIMAL(12,2), allowNull: false },
   stok: { type: DataTypes.INTEGER, defaultValue: 0 },
+  umkm_id: { type: DataTypes.INTEGER, allowNull: true },
   aktif: { type: DataTypes.BOOLEAN, defaultValue: true }
 }, {
   tableName: "PRODUK",
@@ -18,5 +20,8 @@ const Produk = sequelize.define("Produk", {
 
 Produk.belongsTo(JenisProduk, { foreignKey: "jenis_produk_id" });
 JenisProduk.hasMany(Produk, { foreignKey: "jenis_produk_id" });
+
+Produk.belongsTo(Umkm, { foreignKey: "umkm_id" });
+Umkm.hasMany(Produk, { foreignKey: "umkm_id" })
 
 module.exports = Produk;
--- src/routes/produk.js
+++ src/routes/produk.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/authHandler');
 const produkController = require('../controllers/produkController');
 
-router.get('/', produkController.getAllProduk.bind(produkController));
-router.get('/:id', produkController.getProdukById.bind(produkController));
-router.post('/', produkController.createProduk.bind(produkController));
+
+router.get('/', auth, produkController.getAllProduk.bind(produkController));
+router.get('/jenis', produkController.getJenisProduk.bind(produkController)); 
+router.get('/statistik', produkController.getStatistics.bind(produkController));
+router.get('/:id', auth, produkController.getProdukById.bind(produkController));
+router.post('/', auth, produkController.createProduk.bind(produkController));
 router.put('/:id', produkController.updateProduk.bind(produkController));
 router.delete('/:id', produkController.deleteProduk.bind(produkController));
 
